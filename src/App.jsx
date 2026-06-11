44b8002299d2b6e99671886daf8269815f9f4447
Bhavesh189 <deva.pndit86@gmail.com>
2026-06-11 21:23:58 +0530

    Navbar Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,53 @@ import './App.css'
 import Home from './components/Home.jsx'
 import Registerr from './components/Register.jsx';
 import Login from './components/Login.jsx';
+import Navbar from './components/Navbar.jsx';
+import {createBrowserRouter, Outlet} from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
+import Account from './components/Account.jsx';
+import Courses from './components/Courses.jsx';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
+
+  const routes = createBrowserRouter( [
+    
+    {
+      path : '/',
+      element : (<>
+            <Navbar />
+            <Outlet />
+              </>),
+
+      children : [
+        {
+          path : '/',
+          element : <Home></Home>
+        },
+        
+        {
+          path : '/courses',
+          element : <Courses> </Courses>
+        },
+
+        {
+          path : '/account',
+          element : <Account />
+        }
+      ]
+    }
+  ]
+  )
+
+
+
     const [login, isLogin] = useState(false);
 
   return (
     <>
-      {
-        login ? <Login /> : <Registerr />
-      }
+    <RouterProvider router={routes}>
+    <Navbar/>
+    </RouterProvider>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import './css/Navbar.css'
+import logo from '../assets/logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const navigate = useNavigate();
+
+  const [isOpen, setOpen] = useState(false);
+
+  function handleClick() {
+    setOpen(prev => !prev);
+  }
+
+  function navi(path) {
+    navigate(path);
+    setOpen(false);
+  }
+
+
   return (
-    <div>
+    <div className='nav'>
+      <img src={logo} alt="Logo" />
+
+
+      <FontAwesomeIcon icon={isOpen ? faXmark : faBars} onClick={handleClick} className='bars'/>
       
+      <ul  style={{right : isOpen ? "0" : "-70%"}}>
+        <li onClick={()=> navi('/')}>Home</li>
+        <li onClick={()=> navi('/courses')}>Courses</li>
+        <li onClick={()=> navi('/account')}>Account</li>
+      </ul>
     </div>
   )
 }
